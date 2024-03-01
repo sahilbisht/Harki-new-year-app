37ede5d7680ac1a6fc118016d7f24533efdae2d2
sahilbisht <sahilbisht989@gmail.com>
2024-03-01 15:54:41 +0530

    new update

--- backend/index.js
+++ backend/index.js
@@ -21,22 +21,41 @@ app.post("/todo", async function (req, res) {
   await todo.create({
     title: createPayload.title,
     description: createPayload.description,
+    completed: false,
   });
   res.json({
     msg: "Todo created",
   });
 });
-app.get("/todos", function (req, res) {});
 
-app.put("/completed", function (req, res) {
+app.get("/todos", async function (req, res) {
+  const todos = await todo.find({});
+  res.json({
+    todos,
+  });
+});
+
+app.put("/completed", async function (req, res) {
   const updatePayload = req.body;
   const parsedPayload = updateTodo.safeParse(updatePayload);
   if (!parsedPayload.success) {
     req.status(411).json({
-      msg: "You sent the wrong inputss",
+      msg: "You sent the wrong inputs",
     });
     return;
   }
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  );
+
+  res.json({
+    msg: "Todo marked as completed",
+  });
 });
 
 app.listen(port, () => {
